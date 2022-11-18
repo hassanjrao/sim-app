f8a2ed4ab084edbb44702cfaee6003ea42f945cb
Hassan Javaid <hassanjrao@gmail.com>
2022-11-19 01:29:52 +0400

    initital commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>OneUI - Bootstrap 5 Admin Template &amp; UI Framework</title>
+        <title>Sim App</title>
 
-        <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+        <meta name="description" content="Sim App">
         <meta name="author" content="pixelcave">
         <meta name="robots" content="noindex, nofollow">
 
